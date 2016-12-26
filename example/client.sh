ed484de4d5bf345aaae59b6f0b5ec5d951780b55
wmin0 <wmin0@hotmail.com>
2016-12-26 17:34:42 +0800

    add debug

--- example/client.sh
+++ example/client.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if (( $# < 3 )); then
+  echo $0 web_port local_port server_port
+  exit 1
+fi
+
+DEBUG=multinode:* \
 npm run client \
 --socket.io-adapter-multinode-example:web_port=$1 \
 --socket.io-adapter-multinode-example:local_port=$2 \
--- example/server.sh
+++ example/server.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+if (( $# < 1 )); then
+  echo $0 server_port
+  exit 1
+fi
+
+DEBUG=multinode:* \
 npm run server \
 --socket.io-adapter-multinode-example:server_port=$1
